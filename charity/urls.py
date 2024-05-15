b413452f20a8f659ff60c47be6d07522c119274f
Serhii Dotsenko <serdotsenko@gmail.com>
2024-05-15 09:14:03 +0100

    Deploy on Heroku

--- charity/urls.py
+++ charity/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from donation.views import my_donation
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('donation/', my_donation, name='donation'),
 ]
--- donation/views.py
+++ donation/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def my_donation(request):
+    return HttpResponse("Hello, Charity!")
