38061c5362b29af0011ed1edf3f7600fcf776504
Serhii Dotsenko <serdotsenko@gmail.com>
2024-05-20 12:02:52 +0100

    Added humanize to settings.py, donation item extracted into separate file,

--- donation/views.py
+++ donation/views.py
@@ -1,5 +1,32 @@
+import os
+import random
+
 from django.shortcuts import render
 
 
 def donations(request):
-    return render(request, 'donation/donations.html')
+    images = os.listdir('static/images/causes')
+    donations = [
+        {
+            'image': 'static/images/causes/' + random.choice(images),
+            'title': "Title 1",
+            'description': "Description 1",
+            'raised': 30000,
+            'goal': 50000,
+        },
+        {
+            'image': 'static/images/causes/' + random.choice(images),
+            'title': "Title 2",
+            'description': "Description 2",
+            'raised': 60000,
+            'goal': 70000,
+        },
+        {
+            'image': 'static/images/causes/' + random.choice(images),
+            'title': "Title 3",
+            'description': "Description 3",
+            'raised': 80000,
+            'goal': 100000,
+        },
+    ]
+    return render(request, 'donation/donations.html', {'donations': donations})
