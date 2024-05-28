dae2d9bf4442dd9f870183b3d66e967ea70c1562
Serhii Dotsenko <serdotsenko@gmail.com>
2024-05-28 20:24:40 +0100

    Enhanced input validation for custom donation amount
    Added a keypress event listener to the custom donation amount input field. This enhancement prevents non-numeric characters and leading zeros from being entered, ensuring that the input is always a valid positive integer.

--- static/js/donate.js
+++ static/js/donate.js
@@ -6,3 +6,8 @@ radioInputs.forEach(e => e.addEventListener('click', () =>
 
 customAmountInput.addEventListener('input', () => radioInputs.forEach(radio =>
     radio.checked = radio.nextElementSibling.textContent.trim().replace('$', '') === customAmountInput.value));
+
+customAmountInput.addEventListener('keypress', e => {
+    const char = String.fromCharCode(e.which);
+    (!(/[0-9]/.test(char)) || (customAmountInput.value.length === 0 && char === '0')) && e.preventDefault();
+});
