2a9b420aa6537925892c9601e183a25296d057b4
Serhii Dotsenko <serdotsenko@gmail.com>
2024-06-10 10:33:29 +0100

    Added error handler for Stripe processins transaction.

--- donate/views.py
+++ donate/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import get_user
 from django.shortcuts import redirect
+from stripe.error import StripeError
 
 from donate.models import StripeTransaction, Payment
 from donation.models import Donation
@@ -31,26 +32,31 @@ def make_stripe_payment(request):
     stripe.api_key = settings.STRIPE_SK
     user = get_user(request)
 
-    return stripe.Charge.create(
-        amount=int(request.POST['custom-amount']) * 100,
-        currency='usd',
-        description=Donation.objects.get(id=request.POST['cause']).title,
-        customer=stripe.Customer.create(
-            email=user.email,
-            name=user.username,
-            source=request.POST['stripeToken']
+    try:
+        return stripe.Charge.create(
+            amount=int(request.POST['custom-amount']) * 100,
+            currency='usd',
+            description=Donation.objects.get(id=request.POST['cause']).title,
+            customer=stripe.Customer.create(
+                email=user.email,
+                name=user.username,
+                source=request.POST['stripeToken']
+            )
         )
-    )
+    except StripeError as e:
+        messages.add_message(request, messages.ERROR, str(e))
+        return None
 
 
 def donate(request):
     if request.method == 'POST':
         update_user_details(request)
         charge = make_stripe_payment(request)
+        if charge is None:
+            return redirect('donations')
         save_transaction(request, charge)
         message = (f'Thank you for your donation of '
-                   f'${int(charge["amount"] / 100)}, You may see it in the '
-                   f'first row of this table')
+                   f'${int(charge["amount"] / 100)}, You may see it in '
+                   f'the first row of this table')
         messages.add_message(request, messages.SUCCESS, message)
-
-    return redirect('history')
+        return redirect('history')
