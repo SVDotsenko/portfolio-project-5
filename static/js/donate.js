fedaf98ffb38683ad17683232ee1ec5041247612
Serhii Dotsenko <serdotsenko@gmail.com>
2024-06-14 09:12:39 +0100

    Adjusted adaptive disign for history page

--- static/js/donate.js
+++ static/js/donate.js
@@ -7,11 +7,6 @@ radioInputs.forEach(e => e.addEventListener('click', () =>
 customAmountInput.addEventListener('input', () => radioInputs.forEach(radio =>
     radio.checked = radio.nextElementSibling.textContent.trim().replace('$', '') === customAmountInput.value));
 
-customAmountInput.addEventListener('keypress', e => {
-    const char = String.fromCharCode(e.which);
-    (!(/[0-9]/.test(char)) || (customAmountInput.value.length === 0 && char === '0')) && e.preventDefault();
-});
-
 const collapsePersonalInfo = () => {
     if (['email', 'first-name', 'last-name'].reduce((acc, curr) => acc && document.getElementById(curr).value, true)) {
         setTimeout(() => new bootstrap.Collapse(document.getElementById('collapseOne'), {
